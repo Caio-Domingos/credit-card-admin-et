6f420a792b658d813ddd969682e79674c06a60ee
Caio Domingos <caiod.dev@gmail.com>
2021-01-19 22:50:05 -0300

    Insert bulma on project

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+require('@/assets/bulma.scss');
+
 library.add(faPlus);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
